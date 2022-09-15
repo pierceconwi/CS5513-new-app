ba3c53961a3478608856b61656259fe2f273ec4f
Pierce Conwi <pierceconwi@gmail.com>
2022-09-14 22:03:04 -0700

    second json file rendering

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,37 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { getSortedList } from '../lib/data';
+import { getSortedList2 } from '../lib/data';
 import Link from 'next/link';
 import Layout from '../components/layout.js';
 
 export async function getStaticProps() {
-  const allData = getSortedList();
+  const firstData = getSortedList();
+  const secondData = getSortedList2();
   return {
     props: {
-      allData
+      firstData,
+      secondData
     }
   };
 }
 
-export default function Home({ allData }) {
+export default function Home({ firstData, secondData }) {
   return (
     <Layout>
     <h1>App Name</h1>
     <p>version 0.5</p>
     <p>by Pierce Conwi</p>
     <div>
-      {allData.map(({ id, name }) => (
+      {firstData.map(({ id, name }) => (
+        <Link key={id} href={`/pmc-pages/${id}`}>
+              <a className="list-group-item list-group-item-action">{name}</a>
+        </Link>
+      ))}
+    </div>
+    <br></br>
+    <div>
+      {secondData.map(({ id, name }) => (
         <Link key={id} href={`/pmc-pages/${id}`}>
               <a className="list-group-item list-group-item-action">{name}</a>
         </Link>
